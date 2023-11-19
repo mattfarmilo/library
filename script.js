bb5443a112e5f7e327ab2c7e3ee9d08901c9cd4a
mattfarmilo <longrnr56@gmail.com>
2023-11-19 15:49:03 +1100

    Restore previous work after git error
    Restored local files to git after git error.  Need to add button to
    remove books from library

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
+//test books
 const workingGenius = new Book("The 6 Types of Working Genius", "Patrick Lencioni", 228, true);
 addBookToLibrary(workingGenius);
 const bono = new Book("Bono", "Michka Assayas", 323, false);
@@ -25,28 +26,56 @@ const disciples = new Book("Disciples Who Will Last", "Tim Hawkins", 208, true);
 addBookToLibrary(disciples);
 
 //display library
-const library = document.querySelector('.library');
+function displayLibrary() {
+  const library = document.querySelector('.library');
 
-myLibrary.forEach((record) => {
-  const book = document.createElement('div');
-  book.classList.add('book');
-  const title = document.createElement('div');
-  title.classList.add('title');
-  const author = document.createElement('div');
-  author.classList.add('author');
-  const pages = document.createElement('div');
-  pages.classList.add('pages');
-  const read = document.createElement('div');
-  read.classList.add('read');
-  
-  title.textContent = record.title;
-  author.textContent = `by ${record.author}`;
-  pages.textContent = `${record.pages} pages`;
-  read.textContent = record.read;
+  var child = library.lastElementChild;
+  while (child) {
+    library.removeChild(child);
+    child = library.lastElementChild;
+  }
 
-  book.appendChild(title);
-  book.appendChild(author);
-  book.appendChild(pages);
-  book.appendChild(read);
-  library.appendChild(book);
-});
+  myLibrary.forEach((record) => {
+    const book = document.createElement('div');
+    book.classList.add('book');
+    const title = document.createElement('div');
+    title.classList.add('title');
+    const author = document.createElement('div');
+    author.classList.add('author');
+    const pages = document.createElement('div');
+    pages.classList.add('pages');
+    const read = document.createElement('div');
+    read.classList.add('read');
+    
+    title.textContent = record.title;
+    author.textContent = `by ${record.author}`;
+    pages.textContent = `${record.pages} pages`;
+    read.textContent = record.read;
+
+    book.appendChild(title);
+    book.appendChild(author);
+    book.appendChild(pages);
+    book.appendChild(read);
+    library.appendChild(book);
+  });
+};
+
+//submit new book form
+const submit = document.querySelector('#submit');
+
+submit.addEventListener("click", newBook);
+
+function newBook(event) {
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  const pages = document.querySelector('#pages').value;
+  const read = document.querySelector('input[name="read"]:checked').value === 'yes'
+
+  const newBook = new Book(title, author, pages, read);
+  addBookToLibrary(newBook);
+  displayLibrary();
+  document.querySelector('#form').reset();
+  event.preventDefault();
+}
+
+displayLibrary();
